2872f7baa6d9aba3d0011c87cf6611d0b3bad429
Linus Etyang Murunga <162434996+etyanglinus@users.noreply.github.com>
2024-10-16 10:56:35 +0300

    Create resources.tsx

---
+++ src/types/resources.tsx
@@ -0,0 +1,15 @@
+
+export interface Course {
+  title: string;
+  link: string;
+}
+
+export interface Workshop {
+  title: string;
+  link: string;
+}
+
+export interface FAQ {
+  question: string;
+  answer: string;
+}
